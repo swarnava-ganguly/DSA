8ee2e4bfa80388564452d7bd6b313eb916df8be9
Your Name <you@example.com>
2023-12-07 22:47:37 +0530

    wip

--- arrays/containsDuplicate.py
+++ arrays/containsDuplicate.py
@@ -1,4 +1,5 @@
 # Leetcode - https://leetcode.com/problems/contains-duplicate/description/
+# Complexity - O(n)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         dect = {}
